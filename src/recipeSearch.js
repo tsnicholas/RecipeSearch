9bed6c5a7d0dee2632e51ef71fe2d22a30728d7f
Timothy Nicholas <tsnicholas@bsu.edu>
2023-11-09 13:59:56 -0500

    Get data from API

--- src/recipeSearch.js
+++ src/recipeSearch.js
@@ -1,15 +1,59 @@
 import $ from "jQuery";
+const jsonpath = require("jsonpath");
 
-$.ajax({
-    url: 'https://www.themealdb.com/api/json/v1/1/search.php',
-    type: 'GET',
-    data: {s: "Arrabiata"},
-    dataType: 'json',
-    CORS: true,
-    success: function() {
-        console.log("Got data!");
-    },
-    error: function () {
-        console.log("Something went wrong!");
+const searchButton = $("#searchButton");
+searchButton.on("click", search);
+
+async function search() {
+    var ingredient = $("#searchInput").val().replace(" ", "_");
+    console.log(ingredient);
+    var queryResult = await getJSON('https://www.themealdb.com/api/json/v1/1/filter.php', {i: ingredient});
+    var mealIds = jsonpath.query(queryResult, "$.meals..idMeal");
+    var recipes = await getRecipes(mealIds);
+    console.log(recipes[0].ingredients);
+}
+
+async function getJSON(url, data) {
+    return await $.ajax({
+        url: url,
+        type: 'GET',
+        data: data,
+        dataType: 'json',
+        CORS: true,
+    }).done((data, textStatus, jqXHR) => {
+        console.log(data);
+        console.log(textStatus);
+        console.log(jqXHR.responseText);
+        return JSON.parse(jqXHR.responseText);
+    });
+}
+
+async function getRecipes(mealIds) {
+    const recipes = [];
+    for(var mealId of mealIds) {
+        var mealJson = await getJSON('https://www.themealdb.com/api/json/v1/1/lookup.php', {i: mealId});
+        var meal = mealJson.meals[0];
+        console.log(JSON.stringify(meal));
+        var recipeData = {
+            name: meal.strMeal,
+            thumbnail: meal.strMealThumb,
+            instructions: meal.strInstructions,
+            ingredients: getIngredients(meal),
+        }
+        recipes.push(recipeData);
     }
-});
+    return recipes;
+}
+
+function getIngredients(mealJson) {
+    var ingredients = [];
+    for(let i = 1; i <= 20; i++) {
+        var ingredient = JSON.stringify(jsonpath.query(mealJson, `$.strIngredient${i}`));
+        var measure = JSON.stringify(jsonpath.query(mealJson, `$.strMeasure${i}`));
+        if(measure === "" || measure === null || ingredient === "" || ingredient === null) {
+            break;
+        }
+        ingredients.push(`${measure} ${ingredient}`);
+    }
+    return ingredients;
+}
